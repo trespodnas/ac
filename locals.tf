a5ccecff0ac46633a10199d5158c9d036ee94b93
Jay <jnelson@LTCF99.local>
2024-04-02 11:31:27 -0400

    updated locals for vm size and added tenable repo directory

--- locals.tf
+++ locals.tf
@@ -2,7 +2,7 @@ locals {
   vm_specs = {
     "dilz-acas-prod-securitycenter-ugv-1" = {
       location = "eastus"
-      size     = "Standard_D8s_v3"
+      size     = "Standard_D16ls_v5"
     }
     "dilz-acas-prod-scan-ugv-1" = {
       location = "eastus"
